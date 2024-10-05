c2d3b696a9371cd94281c9095efe93ac2551c16c
GouravMalviya445 <gouravmalviya445@gmail.com>
2024-10-05 15:47:07 +0530

    add config

--- server.ts
+++ server.ts
@@ -1,7 +1,8 @@
 import { app } from './src/app';
+import { config } from './src/config/config';
 
 function startServer() {
-    const port = process.env.PORT || 8000;
+    const port = config.port || 8000;
     app.listen(port, () => {
         console.log(`app is listening at http://localhost:${port}`);
     })
