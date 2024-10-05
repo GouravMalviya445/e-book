8c652e350ed442885968372d543dd7054971b178
GouravMalviya445 <gouravmalviya445@gmail.com>
2024-10-05 15:20:23 +0530

    add express server

--- server.ts
+++ server.ts
@@ -1,1 +1,10 @@
-console.log("welcome to ebook APIs.");
+import { app } from './src/app';
+
+function startServer() {
+    const port = process.env.PORT || 8000;
+    app.listen(port, () => {
+        console.log(`app is listening at http://localhost:${port}`);
+    })
+}
+
+startServer();
